ad3e88d0bc2f7702a246b82a186d0d766d1dfdb1
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-22 22:10:02 +0900

    :bug: 動的グループは管理エージェントサービスとしコンピュートには付与しないため修正

--- envs/iam.tf
+++ envs/iam.tf
@@ -1,41 +1,29 @@
 /************************************************************
-Dynamic Group - Compute
+Dynamic Group - Management Agents
 ************************************************************/
 # 「oci_identity_dynamic_group」を使用する場合はルートコンパートメントのDefaultアイデンティティドメインにしか作成できない
 # 「oci_identity_domains_dynamic_resource_group」を使用すれば、指定のアイデンティティドメインに作成可能
-resource "oci_identity_dynamic_group" "compute_oracle" {
+resource "oci_identity_dynamic_group" "management_agents" {
   compartment_id = var.tenancy_ocid
-  name           = "Compute_Dynamic_Group_Oracle"
-  description    = "Compute Dynamic Group Oracle"
+  name           = "Management_Agents_Dynamic_Group"
+  description    = "Management Agents Dynamic Group"
   matching_rule = format(
-    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'oracle'}",
-    oci_identity_compartment.workload.id
-  )
-  defined_tags = local.common_defined_tags
-}
-
-resource "oci_identity_dynamic_group" "compute_windows" {
-  compartment_id = var.tenancy_ocid
-  name           = "Compute_Dynamic_Group_Windows"
-  description    = "Compute Dynamic Group Windows"
-  matching_rule = format(
-    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'windows'}",
+    "All {resource.type='managementagent', resource.compartment.id='%s'}",
     oci_identity_compartment.workload.id
   )
   defined_tags = local.common_defined_tags
 }
 
 /************************************************************
-IAM Policy - Compute
+IAM Policy - Management Agents
 ************************************************************/
-resource "oci_identity_policy" "compute_for_agent_custommoni" {
+resource "oci_identity_policy" "management_agents_custommoni" {
   compartment_id = oci_identity_compartment.workload.id
-  description    = "OCI Compute Policy for CloudAgent Custom Metrics Monitoring"
-  name           = "compute-cloudagent-custom-metrics-monitoring-policy"
+  description    = "OCI Management Agents Policy for Metrics Monitoring"
+  name           = "management-agents-custom-metrics-monitoring-policy"
   statements = [
-    format("allow dynamic-group %s, %s to use metrics in compartment %s",
-      oci_identity_dynamic_group.compute_oracle.name,
-      oci_identity_dynamic_group.compute_windows.name,
+    format("allow dynamic-group %s to use metrics in compartment %s",
+      oci_identity_dynamic_group.management_agents.name,
       oci_identity_compartment.workload.name
     )
   ]
